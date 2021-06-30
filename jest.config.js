a630d3f38786401771789e2cb9df2a69a6848590
koushiro <koushiro.cqx@gmail.com>
2021-06-30 15:08:15 +0800

    tmp
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ export const testEnvironment = 'node';
 export const transform = {
   '^.+\\.(t|j)sx?$': 'ts-jest'
 };
-export const transformIgnorePatterns = [
-  '/node_modules/(?!(@polkadot|@babel/runtime/helpers/esm/))'
-];
+// export const transformIgnorePatterns = [
+//   '/node_modules/(?!(@polkadot|@babel/runtime/helpers/esm/))'
+// ];
